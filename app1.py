4604f22d3abe9033030410bad9dfd49ffd2caaa9
Nunes6104 <jmmnunes6104@gmail.com>
2025-03-28 10:38:11 +0000

    text

---
+++ app1.py
@@ -0,0 +1,13 @@
+nome = input("Qual é o teu nome? ")
+print("Olá,", nome)
+
+numero = int(input("Digite um numero: "))
+print(f"O numero é: {numero}")
+
+dia = int(input("Qual o dia de nascimento: "))
+mes = int(input("Qual o mes de nascimento: "))
+ano = int(input("Qual o ano de nascimento: "))
+
+diasDeVida = ((2025 - ano)*365) + ((12 - mes)* 30) + (31 - dia)
+
+print(f"O {nome} tu viveste {diasDeVida} dias")
